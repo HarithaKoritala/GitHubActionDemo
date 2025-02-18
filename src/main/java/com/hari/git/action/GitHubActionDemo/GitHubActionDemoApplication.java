d9046c2257722959385b0622bfbfa68dfb73dda1
haritha.k <koritalaharitha123@gmail.com>
2025-02-17 20:34:23 -0600

    changes related workflow

--- src/main/java/com/hari/git/action/GitHubActionDemo/GitHubActionDemoApplication.java
+++ src/main/java/com/hari/git/action/GitHubActionDemo/GitHubActionDemoApplication.java
@@ -2,12 +2,20 @@ package com.hari.git.action.GitHubActionDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GitHubActionDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitHubActionDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubActionDemoApplication.class, args);
+    }
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello...! Your GitAction First Project Succeeded...!";
+    }
 
 }
